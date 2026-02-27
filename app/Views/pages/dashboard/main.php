ee4323e5c3aa04cc7353d560771ffa0f32d5532c
Muhammad Fattahillah. Mz <fattahillahmuhammad48@gmail.com>
2026-02-28 03:56:34 +0700

    Mengatur responsif untuk ukuran smartphone pada halaman dashboard

--- app/Views/pages/dashboard/main.php
+++ app/Views/pages/dashboard/main.php
@@ -10,8 +10,9 @@
 </head>
 
 <body class="grid grid-cols-1 sm:grid-cols-12 min-h-screen">
-    <aside class="fixed sm:block sm:sticky top-0 h-screen sm:h-fit sm:col-span-2 bg-white border-r border-solid border-gray-200 z-[9999999]">
-        <div class="top-aside pt-6 pb-4 px-6 tracking-wider">
+    <aside class="fixed sm:block sm:sticky top-0 h-screen sm:h-fit sm:col-span-2 border-r border-solid border-gray-200 z-[9999999]">
+        <div class="nav-overlay sm:hidden absolute top-0 left-0 w-screen h-screen bg-black/35 z-[-1]"></div>
+        <div class="top-aside bg-white pt-6 pb-4 px-6 tracking-wider">
             <h2 class="text-xl text-green">SiMELEK</h2>
             <p class="mt-0.5 text-xs text-gray-500/90 text-pretty">Sistem Informasi Media Elektronik</p>
         </div>
@@ -24,7 +25,7 @@
                 <p class="text-xs text-gray-500/90">User</p>
             </div>
         </div>
-        <nav class="p-4 flex flex-col gap-2 min-h-[230px] max-h-[230px] overflow-y-auto border-b border-solid border-gray-200">
+        <nav class="bg-white p-4 flex flex-col gap-2 min-h-[230px] max-h-[230px] overflow-y-auto border-b border-solid border-gray-200">
             <a href="/dashboard" class="beranda font-text active p-3 flex gap-1.5 bg-blue-600 text-white rounded-lg">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
@@ -53,7 +54,7 @@
                 <span class="truncate" title="Riwayat Pengajuan">Profil</span>
             </a>
         </nav>
-        <div class="mores-nav p-4 flex flex-col gap-2">
+        <div class="mores-nav bg-white p-4 h-full flex flex-col gap-2">
             <a href="/dashboard/pengaturan" class="pengaturan font-text active p-3 flex gap-1.5 text-gray-500/90 rounded-lg transition duration-150 ease-in hover:bg-gray-200">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z" />
