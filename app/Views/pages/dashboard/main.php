481435e62c6dd369fe58a17091407ae0f1501f20
Muhammad Fattahillah. Mz <fattahillahmuhammad48@gmail.com>
2026-03-01 04:42:19 +0700

    Memperbaiki tipografi pada navigasi halaman dashboard

--- app/Views/pages/dashboard/main.php
+++ app/Views/pages/dashboard/main.php
@@ -41,28 +41,28 @@
                 </div>
             </div>
             <nav class="scrollbar-custom p-4 flex flex-col gap-2 min-h-[230px] max-h-[230px] overflow-y-auto border-b border-solid border-gray-200">
-                <a href="<?= $current_nav === 'Dashboard' ? 'javascript:void(0)' : '/dashboard' ?>" class="beranda font-text active p-3 flex gap-1.5 rounded-lg <?= $current_nav === 'Dashboard' ? 'font-semibold bg-blue-600 text-white' : 'text-gray-500/90 transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200' ?>">
+                <a href="<?= $current_nav === 'Dashboard' ? 'javascript:void(0)' : '/dashboard' ?>" class="beranda font-text text-sm active p-3 flex items-center font-semibold gap-1.5 rounded-lg <?= $current_nav === 'Dashboard' ? 'bg-blue-600 text-white' : 'text-gray-500/90 transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200' ?>">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                     </svg>
                     <span class="truncate" title="Dashboard">Dashboard</span>
                 </a>
 
-                <a href="<?= $current_nav === 'Pengajuan' ? 'javascript:void(0)' : '/dashboard/pengajuan' ?>" class="beranda font-text active p-3 flex gap-1.5 rounded-lg <?= $current_nav === 'Pengajuan' ? 'font-semibold bg-blue-600 text-white' : 'text-gray-500/90 transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200' ?>">
+                <a href="<?= $current_nav === 'Pengajuan' ? 'javascript:void(0)' : '/dashboard/pengajuan' ?>" class="pengajuan font-text text-sm active p-3 flex items-center font-semibold gap-1.5 rounded-lg <?= $current_nav === 'Pengajuan' ? 'bg-blue-600 text-white' : 'text-gray-500/90 transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200' ?>">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
                     </svg>
                     <span class="truncate" title="Pengajuan">Pengajuan</span>
                 </a>
 
-                <a href="<?= $current_nav === 'Riwayat Pengajuan' ? 'javascript:void(0)' : '/dashboard/riwayat-pengajuan' ?>" class="beranda font-text active p-3 flex gap-1.5 rounded-lg <?= $current_nav === 'Riwayat Pengajuan' ? 'font-semibold bg-blue-600 text-white' : 'text-gray-500/90 transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200' ?>">
+                <a href="<?= $current_nav === 'Riwayat Pengajuan' ? 'javascript:void(0)' : '/dashboard/riwayat-pengajuan' ?>" class="riwayat-pengajuan font-text text-sm active p-3 flex items-center font-semibold gap-1.5 rounded-lg <?= $current_nav === 'Riwayat Pengajuan' ? 'bg-blue-600 text-white' : 'text-gray-500/90 transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200' ?>">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m7.875 14.25 1.214 1.942a2.25 2.25 0 0 0 1.908 1.058h2.006c.776 0 1.497-.4 1.908-1.058l1.214-1.942M2.41 9h4.636a2.25 2.25 0 0 1 1.872 1.002l.164.246a2.25 2.25 0 0 0 1.872 1.002h2.092a2.25 2.25 0 0 0 1.872-1.002l.164-.246A2.25 2.25 0 0 1 16.954 9h4.636M2.41 9a2.25 2.25 0 0 0-.16.832V12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 12V9.832c0-.287-.055-.57-.16-.832M2.41 9a2.25 2.25 0 0 1 .382-.632l3.285-3.832a2.25 2.25 0 0 1 1.708-.786h8.43c.657 0 1.281.287 1.709.786l3.284 3.832c.163.19.291.404.382.632M4.5 20.25h15A2.25 2.25 0 0 0 21.75 18v-2.625c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125V18a2.25 2.25 0 0 0 2.25 2.25Z" />
                     </svg>
                     <span class="truncate" title="Riwayat Pengajuan">Riwayat Pengajuan</span>
                 </a>
 
-                <a href="<?= $current_nav === 'Riwayat Hapus' ? 'javascript:void(0)' : '/dashboard/riwayat-hapus' ?>" class="beranda font-text active p-3 flex gap-1.5 rounded-lg <?= $current_nav === 'Riwayat Hapus' ? 'font-semibold bg-blue-600 text-white' : 'text-gray-500/90 transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200' ?>">
+                <a href="<?= $current_nav === 'Riwayat Hapus' ? 'javascript:void(0)' : '/dashboard/riwayat-hapus' ?>" class="riwayat-hapus font-text text-sm active p-3 flex items-center font-semibold gap-1.5 rounded-lg <?= $current_nav === 'Riwayat Hapus' ? 'bg-blue-600 text-white' : 'text-gray-500/90 transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200' ?>">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                     </svg>
@@ -70,18 +70,18 @@
                 </a>
             </nav>
             <div class="mores-nav p-4 flex flex-col gap-2">
-                <a href="/dashboard/pengaturan" class="pengaturan font-text active p-3 flex gap-1.5 text-gray-500/90 rounded-lg transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200" aria-label="Kunjungi halaman pengaturan">
+                <a href="/dashboard/pengaturan" class="pengaturan font-text active p-3 flex items-center gap-1.5 text-gray-500/90 rounded-lg transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200" aria-label="Kunjungi halaman pengaturan">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z" />
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
-                    <span class="font-semibold truncate" title="Pengaturan">Pengaturan</span>
+                    <span class="font-semibold truncate text-sm" title="Pengaturan">Pengaturan</span>
                 </a>
-                <a href="/dashboard/logout" class="logout active font-text p-3 flex gap-1.5 text-red-500 rounded-lg transition duration-150 ease-in hover:bg-red-100 focus:outline-none focus:bg-red-100" aria-label="Keluar">
+                <a href="/dashboard/logout" class="logout active font-text p-3 flex items-center gap-1.5 text-red-500 rounded-lg transition duration-150 ease-in hover:bg-red-100 focus:outline-none focus:bg-red-100" aria-label="Keluar">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9" />
                     </svg>
-                    <span class="font-semibold truncate" title="Pengaturan">Keluar</span>
+                    <span class="font-semibold truncate text-sm" title="Pengaturan">Keluar</span>
                 </a>
             </div>
         </div>
