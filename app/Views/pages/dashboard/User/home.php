6c2b26a247319c543e4dbd5cc7326e80d6d26c1c
fattahillah <fattahillahmuhammad48@gmail.com>
2026-03-02 11:32:00 +0700

    Memperbaiki kesalahan pada link Kelola Pengajuan dalam Aksi Cepat

--- app/Views/pages/dashboard/User/home.php
+++ app/Views/pages/dashboard/User/home.php
@@ -215,7 +215,7 @@
                 </svg>
                 <span class="font-text font-semibold text-sm">Buat Pengajuan Baru</span>
             </a>
-            <a href="/dashboard/pengajuan" class="py-3.5 px-3 flex justify-center md:justify-start items-center gap-1.5 border border-solid border-gray-300 rounded-md transition duration-150 hover:bg-primary">
+            <a href="/dashboard/riwayat-pengajuan" class="py-3.5 px-3 flex justify-center md:justify-start items-center gap-1.5 border border-solid border-gray-300 rounded-md transition duration-150 hover:bg-primary">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
                 </svg>
