3285087de04102131cdc2cdfb72f05f2c1fb924d
fattahillah <fattahillahmuhammad48@gmail.com>
2026-03-03 07:26:45 +0700

    Menerapkan component card ke Dashboard User

--- app/Views/components/card.php
+++ app/Views/components/card.php
@@ -1,8 +1,8 @@
 <?php foreach ($items as $card): ?>
     <?php
-    $set_width = $card["width"] ?? '';
+    $add_class = $card["add_class"] ?? '';
     ?>
-    <div class="total-pengajuan-baru p-7 flex items-center gap-8 rounded-lg shadow-md <?= $set_width ?>">
+    <div class="total-pengajuan-baru p-7 flex items-center gap-8 rounded-lg shadow-md <?= $add_class ?>">
         <div class="card-text flex flex-col gap-1">
             <p class="font-semibold text-sm text-gray-500/90"><?= $card["card_title"] ?></p>
             <span class="font-text font-semibold text-3xl"><?= $card["card_description"] ?></span>
