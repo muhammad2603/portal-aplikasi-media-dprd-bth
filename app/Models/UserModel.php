584d922ed9023179f8e0f71be58cceffcf0083d8
fattahillah <fattahillahmuhammad48@gmail.com>
2026-03-22 23:43:02 +0700

    Menambahkan method pengecekan ketersediaan email ditable user

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,11 +1,28 @@
 <?php
-
+// namespace Models
 namespace App\Models;
-
+// use Model from codeigniter
 use CodeIgniter\Model;
-
+// @class
 class UserModel extends Model
 {
+    // @set table name
     protected $table = "user";
+    // @set allowed fields
     protected $allowedFields = ["email", "password", "role_id", "is_verified", "token_login"];
+    /**
+     * @method isEmailExist
+     * 
+     * @param string $email
+     * 
+     * @return true jika email tersedia
+     */
+    public function isEmailExist(string $email)
+    {
+        $find_email = $this->select("email")->where("email", $email)->first();
+        // @if email exist on DB
+        if ($find_email !== null)
+            // @return true
+            return true;
+    }
 }
