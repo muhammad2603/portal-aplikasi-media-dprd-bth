14a7d28e827ef93ef74d7820cd7759416baa528d
fattahillah <fattahillahmuhammad48@gmail.com>
2026-03-22 23:38:25 +0700

    Mengubah struktur formulir pendaftaran

--- app/Views/pages/register.php
+++ app/Views/pages/register.php
@@ -1,9 +1,8 @@
 <?= $this->extend('pages/main') ?>
-
 <?= $this->section('content') ?>
 <main class="px-6 sm:px-14 xl:px-0 w-full min-h-screen flex items-center">
     <section class="login grid grid-cols-1 lg:grid-cols-2 gap-12 mx-auto p-8 md:p-12 lg:p-8 xl:p-4 2xl:py-10 2xl:px-8 w-full xl:w-3/4 h-auto bg-white shadow-md rounded-xl">
-        <aside class="background-login hidden lg:block w-full h-full p-4 bg-[url(https://cdn.antaranews.com/cache/1200x800/2023/02/01/IMG-20230201-WA0025_3.jpg)] bg-cover bg-center bg-no-repeat border-2 border-solid border-primary rounded-lg">
+        <aside class="background-login hidden lg:block w-full h-[600px] p-4 bg-[url(https://cdn.antaranews.com/cache/1200x800/2023/02/01/IMG-20230201-WA0025_3.jpg)] bg-cover bg-center bg-no-repeat border-2 border-solid border-primary rounded-lg">
             <figure>
                 <img src="<?= base_url('/assets/images/logo-batanghari.png') ?>" alt="Logo Kabupaten Batang Hari" class="w-10" />
             </figure>
@@ -13,7 +12,8 @@
             <p class="text-center text-sm text-pretty text-gray-500/90 tracking-wide">Silahkan daftar diri anda untuk mengakses aplikasi Sistem Informasi Media Elektronik Setwan Kabupaten Batang Hari!</p>
             <div class="form-login mt-2 grid grid-cols-1 sm:grid-cols-2 gap-2.5">
                 <div class="input-nama-lengkap sm:col-span-2 flex flex-col gap-1.5 text-sm">
-                    <label for="namaLengkap" class="font-text font-semibold"><span>Nama Lengkap</span></label>
+                    <label for="namaLengkap" class="w-fit font-text font-semibold"><span>Nama Lengkap</span></label>
+                    <span id="errorInputNamaLengkap" class="error pl-1.5 font-text font-semibold text-xs text-red-500 tracking-wide"></span>
                     <div class="input relative text-gray-500/90">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                             class="size-5 absolute top-[50%] left-3 -translate-y-[50%]">
@@ -23,7 +23,7 @@
                     </div>
                 </div>
                 <div class="input-email flex flex-col gap-1.5 text-sm">
-                    <label for="email" class="font-text font-semibold"><span>Email</span></label>
+                    <label for="email" class="w-fit font-text font-semibold"><span>Email</span></label>
                     <div class="input relative text-gray-500/90">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                             class="size-5 absolute top-[50%] left-3 -translate-y-[50%]">
@@ -31,9 +31,10 @@
                         </svg>
                         <input type="email" name="_email" id="email" class="py-2.5 pl-10 pr-2 w-full bg-primary border border-solid border-gray-500/90 rounded-md focus:outline-none" placeholder="nama_email@gmail.com" aria-label="Masukkan Email" autocomplete="email" />
                     </div>
+                    <span id="errorInputEmail" class="error pl-1.5 font-text font-semibold text-xs text-red-500 tracking-wide"></span>
                 </div>
                 <div class="input-no-telp flex flex-col gap-1.5 text-sm">
-                    <label for="noTelp" class="font-text font-semibold"><span>No Telpon/WA</span></label>
+                    <label for="noTelp" class="w-fit font-text font-semibold"><span>No Telpon/WA</span></label>
                     <div class="input relative text-gray-500/90">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                             class="size-5 absolute top-[50%] left-3 -translate-y-[50%]">
@@ -41,9 +42,11 @@
                         </svg>
                         <input type="tel" name="_no_telp" id="noTelp" class="py-2.5 pl-10 pr-2 w-full bg-primary border border-solid border-gray-500/90 rounded-md focus:outline-none" placeholder="08XXXXXXXXXX" aria-label="Masukkan Kata Sandi" autocomplete="tel" />
                     </div>
+                    <span id="errorInputNoTelp" class="error pl-1.5 font-text font-semibold text-xs text-red-500 tracking-wide"></span>
                 </div>
                 <div class="input-password sm:col-span-2 flex flex-col gap-1.5 text-sm">
-                    <label for="password" class="font-text font-semibold"><span>Kata Sandi</span></label>
+                    <label for="password" class="w-fit font-text font-semibold"><span>Kata Sandi</span></label>
+                    <span id="errorInputPassword" class="error pl-1.5 font-text font-semibold text-xs text-red-500 tracking-wide"></span>
                     <div class="input relative text-gray-500/90">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                             class="size-5 absolute top-[50%] left-3 -translate-y-[50%]">
@@ -52,7 +55,25 @@
                         <input type="password" name="_password" id="password" class="py-2.5 px-10 w-full bg-primary border border-solid border-gray-500/90 rounded-md focus:outline-none" placeholder="Kata Sandi" aria-label="Masukkan Kata Sandi" autocomplete="off" />
                     </div>
                 </div>
-                <button type="button" class="sm:col-span-2 mt-1 py-2.5 bg-green font-text font-semibold text-white tracking-wider rounded-md hover:shadow-lg active:shadow-lg">Daftar</button>
+                <div class="input-confirm-password sm:col-span-2 flex flex-col gap-1.5 text-sm">
+                    <label for="confirmPassword" class="w-fit font-text font-semibold"><span>Konfirmasi Kata Sandi</span></label>
+                    <span id="errorInputConfirmPassword" class="error pl-1.5 font-text font-semibold text-xs text-red-500 tracking-wide"></span>
+                    <div class="input relative text-gray-500/90">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                            class="size-5 absolute top-[50%] left-3 -translate-y-[50%]">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                        </svg>
+                        <input type="password" name="_confirm_password" id="confirmPassword" class="py-2.5 px-10 w-full bg-primary border border-solid border-gray-500/90 rounded-md focus:outline-none" placeholder="Konfirmasi Kata Sandi" aria-label="Masukkan Kata Sandi Kembali" autocomplete="off" />
+                    </div>
+                </div>
+                <button id="btnSubmitRegis" type="button" class="sm:col-span-2 mt-1 py-2.5 flex justify-center items-center gap-2 bg-green font-text font-semibold text-white tracking-wider rounded-md hover:shadow-lg active:shadow-lg">
+                    <span>Daftar</span>
+                    <span id="iconLoading" class="hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-5 animate-spin">
+                            <path d="M12 2 A10 10 0 0 1 22 12" />
+                        </svg>
+                    </span>
+                </button>
             </div>
             <div class="bottom text-gray-500/90">
                 <div class="flex items-center gap-5 py-3.5">
@@ -66,4 +87,5 @@
         </div>
     </section>
 </main>
+<script type="module" src="<?= base_url("/assets/js/register.js") ?>"></script>
 <?= $this->endSection() ?>
