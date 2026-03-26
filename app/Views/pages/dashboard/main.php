9121a0685a1eaae91eacaac36727363fce6b0794
fattahillah <fattahillahmuhammad48@gmail.com>
2026-03-27 02:53:52 +0700

    Mengubah pengambilan role dari controller menjadi session

--- app/Views/pages/dashboard/main.php
+++ app/Views/pages/dashboard/main.php
@@ -177,7 +177,7 @@ helper("cookie");
                 </figure>
                 <div class="text-xs flex-1 min-w-0 flex flex-col justify-center gap-0.5">
                     <p class="truncate text-sm font-semibold"><?= session()->get('userFullName') ?></p>
-                    <p class="text-xs text-gray-500/90"><?= $role ?></p>
+                    <p class="text-xs text-gray-500/90"><?= session()->get("role") ?></p>
                 </div>
             </div>
             <nav class="scrollbar-custom p-4 flex flex-col gap-2 min-h-[230px] max-h-[230px] overflow-y-auto border-b border-solid border-gray-200">
@@ -187,29 +187,25 @@ helper("cookie");
                     </svg>
                     <span class="truncate" title="Dashboard">Dashboard</span>
                 </a>
-
                 <a href="<?= $navigation === 'Pengajuan' ? 'javascript:void(0)' : '/dashboard/pengajuan' ?>" class="pengajuan font-text text-sm p-3 flex items-center font-semibold gap-1.5 rounded-lg <?= $navigation === 'Pengajuan' ? 'bg-blue-600 text-white' : 'text-gray-500/90 transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200' ?>">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
                     </svg>
                     <span class="truncate" title="Pengajuan">Pengajuan</span>
                 </a>
-
                 <a href="<?= $navigation === 'Riwayat Pengajuan' ? 'javascript:void(0)' : '/dashboard/riwayat-pengajuan' ?>" class="riwayat-pengajuan font-text text-sm p-3 flex items-center font-semibold gap-1.5 rounded-lg <?= $navigation === 'Riwayat Pengajuan' ? 'bg-blue-600 text-white' : 'text-gray-500/90 transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200' ?>">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m7.875 14.25 1.214 1.942a2.25 2.25 0 0 0 1.908 1.058h2.006c.776 0 1.497-.4 1.908-1.058l1.214-1.942M2.41 9h4.636a2.25 2.25 0 0 1 1.872 1.002l.164.246a2.25 2.25 0 0 0 1.872 1.002h2.092a2.25 2.25 0 0 0 1.872-1.002l.164-.246A2.25 2.25 0 0 1 16.954 9h4.636M2.41 9a2.25 2.25 0 0 0-.16.832V12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 12V9.832c0-.287-.055-.57-.16-.832M2.41 9a2.25 2.25 0 0 1 .382-.632l3.285-3.832a2.25 2.25 0 0 1 1.708-.786h8.43c.657 0 1.281.287 1.709.786l3.284 3.832c.163.19.291.404.382.632M4.5 20.25h15A2.25 2.25 0 0 0 21.75 18v-2.625c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125V18a2.25 2.25 0 0 0 2.25 2.25Z" />
                     </svg>
                     <span class="truncate" title="Riwayat Pengajuan">Riwayat Pengajuan</span>
                 </a>
-
                 <a href="<?= $navigation === 'Aktivitas' ? 'javascript:void(0)' : '/dashboard/aktivitas' ?>" class="aktivitas font-text text-sm p-3 flex items-center font-semibold gap-1.5 rounded-lg <?= $navigation === 'Aktivitas' ? 'bg-blue-600 text-white' : 'text-gray-500/90 transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200' ?>">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
                     <span class="truncate" title="Aktivitas">Aktivitas</span>
                 </a>
-
-                <?php if ($role === 'User'): ?>
+                <?php if (session()->get("role") === 'User'): ?>
                     <a href="<?= $navigation === 'Riwayat Hapus' ? 'javascript:void(0)' : '/dashboard/riwayat-hapus' ?>" class="riwayat-hapus font-text text-sm p-3 flex items-center font-semibold gap-1.5 rounded-lg <?= $navigation === 'Riwayat Hapus' ? 'bg-blue-600 text-white' : 'text-gray-500/90 transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200' ?>">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
@@ -217,8 +213,7 @@ helper("cookie");
                         <span class="truncate" title="Riwayat Hapus">Riwayat Hapus</span>
                     </a>
                 <?php endif ?>
-
-                <?php if ($role === 'Admin'): ?>
+                <?php if (session()->get("role") === 'Admin'): ?>
                     <a href="<?= $navigation === 'Riwayat Pembatalan' ? 'javascript:void(0)' : '/dashboard/riwayat-pembatalan' ?>" class="riwayat-pembatalan font-text text-sm p-3 flex items-center font-semibold gap-1.5 rounded-lg <?= $navigation === 'Riwayat Pembatalan' ? 'bg-blue-600 text-white' : 'text-gray-500/90 transition duration-150 ease-in hover:bg-gray-200 focus:outline-none focus:bg-gray-200' ?>">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
@@ -356,7 +351,7 @@ helper("cookie");
                     </div>
                 </div>
                 <!-- Header link Pengajuan -->
-                <?php if ($role === "User" && $navigation !== 'Pengajuan'): ?>
+                <?php if (session()->get("role") === "User" && $navigation !== 'Pengajuan'): ?>
                     <a href="/dashboard/pengajuan" class="hidden lg:flex py-2 px-2.5 gap-1 bg-blue-500 text-white rounded-md transition duration-150 hover:bg-blue-600 focus:outline-none focus:bg-blue-600" aria-label="Buat pengajuan baru">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
