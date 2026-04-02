b23d85af361aad429a75cc73765fc7fe4644a5df
fattahillah <fattahillahmuhammad48@gmail.com>
2026-04-02 23:20:23 +0700

    Memperbaiki method delete pada endpoint /dashboard/hapus-pengajuan dan kesalahan penulisan pada method createPengajuan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -54,7 +54,8 @@ $routes->group('', ["filter" => VerifiedFilter::class], function ($routes) {
     $routes->get('/dashboard', 'Dashboard::home');
     // @GET dashboard/pengajuan
     $routes->get('/dashboard/pengajuan', 'Dashboard::pengajuan');
-    $routes->post('/dashboard/tambah-pengajuan', 'CRUD_API::createPengajuan');
+    $routes->post('/dashboard/tambah-pengajuan', 'API_CRUD::createPengajuan');
+    $routes->delete('/dashboard/hapus-pengajuan', 'API_CRUD::deletePengajuan');
     // @GET dashboard/riwayat-pengajuan
     $routes->get('/dashboard/riwayat-pengajuan', 'Dashboard::riwayatPengajuan');
     // @GET dashboard/riwayat-hapus
