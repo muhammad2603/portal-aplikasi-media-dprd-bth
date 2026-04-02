9672a0f5c78f101e621b69a9d9b8d983ca5b9322
fattahillah <fattahillahmuhammad48@gmail.com>
2026-04-03 02:05:05 +0700

    Mengubah urutan card total pengajuan

--- app/Views/pages/dashboard/User/home.php
+++ app/Views/pages/dashboard/User/home.php
@@ -31,14 +31,14 @@ $histories_pengajuan = $pengajuanModel->getHistoriesIn7Days($user_id);
                 ]
             ],
             [
-                "card_title" => 'Disetujui',
-                "card_description" => $total_pengajuan_by_status["disetujui"],
+                "card_title" => 'Pending',
+                "card_description" => $total_pengajuan_by_status["pending"],
                 "add_class" => 'shrink-0',
                 "card_icon" => [
                     "icon" => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>',
-                    "icon_color" => 'bg-green-100/80 text-green-600'
+                    "icon_color" => 'bg-amber-100/80 text-amber-600'
                 ]
             ],
             [
@@ -64,14 +64,14 @@ $histories_pengajuan = $pengajuanModel->getHistoriesIn7Days($user_id);
                 ]
             ],
             [
-                "card_title" => 'Pending',
-                "card_description" => $total_pengajuan_by_status["pending"],
+                "card_title" => 'Disetujui',
+                "card_description" => $total_pengajuan_by_status["disetujui"],
                 "add_class" => 'shrink-0',
                 "card_icon" => [
                     "icon" => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>',
-                    "icon_color" => 'bg-amber-100/80 text-amber-600'
+                    "icon_color" => 'bg-green-100/80 text-green-600'
                 ]
             ],
         ]
