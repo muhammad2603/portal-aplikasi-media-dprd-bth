6907049f1a3162d70a91cc46bc0e6a5db0bab37d
fattahillah <fattahillahmuhammad48@gmail.com>
2026-04-04 20:56:20 +0700

    Memperbaiki bug pada filter dan search pengajuan, yang dimana Pengajuan dengan status Pending tidak disertakan.

--- app/Controllers/API_CRUD.php
+++ app/Controllers/API_CRUD.php
@@ -224,7 +224,7 @@ class API_CRUD extends BaseController
             ->join("status", "status.id = sp.id_status")
             ->join("user_meta um", "um.user_id = pengajuan.user_id")
             ->join("riwayat_status_pengajuan rsp", "rsp.id_pengajuan = pengajuan.id")
-            ->join("admin adm", "adm.id = sp.admin_id")
+            ->join("admin adm", "adm.id = sp.admin_id", "LEFT")
             ->join($this->rsp_last, "rsp_last.id_pengajuan = pengajuan.id", "LEFT")
             ->groupBy("rsp.id_pengajuan")
             ->where("pengajuan.user_id", $user_id)
@@ -269,7 +269,7 @@ class API_CRUD extends BaseController
             ->join("status", "status.id = sp.id_status")
             ->join("user_meta um", "um.user_id = pengajuan.user_id")
             ->join("riwayat_status_pengajuan rsp", "rsp.id_pengajuan = pengajuan.id")
-            ->join("admin adm", "adm.id = sp.admin_id")
+            ->join("admin adm", "adm.id = sp.admin_id", "LEFT")
             ->join($this->rsp_last, "rsp_last.id_pengajuan = pengajuan.id", "LEFT")
             ->groupBy("rsp.id_pengajuan")
             ->where("pengajuan.user_id", session()->get("userId"));
