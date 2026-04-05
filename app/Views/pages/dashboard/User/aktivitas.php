bcb5963d4906c644af46ea6de9587aeac00585e4
fattahillah <fattahillahmuhammad48@gmail.com>
2026-04-05 20:40:43 +0700

    Menampilkan data log aktivitas pengguna dari view

--- app/Views/pages/dashboard/User/aktivitas.php
+++ app/Views/pages/dashboard/User/aktivitas.php
@@ -12,7 +12,6 @@
         <!-- Filter Box wrapper -->
         <div id="btnFilterBox" class="filter-box-wrapper relative shrink-0 ml-auto">
             <!-- Button show filter select -->
-            <!-- Button show filter select -->
             <button id="btnFilterBox" type="button" class="filter-box py-2.5 px-4 w-52 flex justify-between items-center gap-3.5 bg-gray-100 rounded-md shadow-sm focus:outline-none">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 text-gray-500/90">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
@@ -24,7 +23,7 @@
             </button>
             <!-- Filter Select -->
             <div id="filterSelect" class="filter-select w-full p-1.5 absolute top-[120%] translate-y-[-16px] flex flex-col border border-solid border-gray-200 bg-white rounded-md shadow-sm opacity-0 transition duration-350 ease-out visibility-hidden pointer-events-none closed">
-                <button type="button" class="filter-option py-1.5 px-2 flex justify-between items-center bg-gray-100 text-sm rounded-md hover:bg-gray-100 focus:outline-none focus:bg-gray-100 active">
+                <button type="button" data-filter-by="desc" class="filter-option py-1.5 px-2 flex justify-between items-center bg-gray-100 text-sm rounded-md hover:bg-gray-100 focus:outline-none focus:bg-gray-100 active">
                     <span>Terbaru</span>
                     <span>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-4">
@@ -32,7 +31,7 @@
                         </svg>
                     </span>
                 </button>
-                <button type="button" class="filter-option py-1.5 px-2 flex justify-between items-center text-sm rounded-md hover:bg-gray-100 focus:outline-none focus:bg-gray-100">
+                <button type="button" data-filter-by="asc" class="filter-option py-1.5 px-2 flex justify-between items-center text-sm rounded-md hover:bg-gray-100 focus:outline-none focus:bg-gray-100">
                     <span>Terlama</span>
                     <span class="hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-4">
@@ -45,78 +44,12 @@
     </section>
     <!-- Akhir Section Filter -->
     <!-- Catatan aktivitas terbaru -->
-    <div class="activities mt-8 flex flex-col gap-4">
-        <div class="flex gap-3.5">
-            <span class="py-1.5 px-1.5 h-fit bg-gray-200/60 text-green-600 rounded-lg">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
-            </span>
-            <div class="flex flex-col gap-0.5">
-                <h3 class="text-base">Pengajuan disetujui</h3>
-                <p class="text-sm text-gray-500/90">Pengajuan "Liputan Pemilu 2026" Anda telah disetujui Admin</p>
-                <span class="mt-1 flex gap-1 text-gray-500/90 text-xs">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                    5 hari yang lalu
-                </span>
-            </div>
-        </div>
-        <div class="flex gap-3.5">
-            <span class="py-1.5 px-1.5 h-fit bg-gray-200/60 text-red-500 rounded-lg">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
-            </span>
-            <div class="flex flex-col gap-0.5">
-                <h3 class="text-base">Pengajuan ditolak</h3>
-                <p class="text-sm text-gray-500/90">Pengajuan "Festival Budaya Nusantara" Anda telah ditolak Admin. Lihat alasan penolakan di Pengajuan anda</p>
-                <span class="mt-1 flex gap-1 text-gray-500/90 text-xs">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                    7 hari yang lalu
-                </span>
-            </div>
-        </div>
-        <div class="flex gap-3.5">
-            <span class="py-1.5 px-1.5 h-fit bg-gray-200/60 text-amber-500 rounded-lg">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
-            </span>
-            <div class="flex flex-col gap-0.5">
-                <h3 class="text-base">Pengajuan terkirim</h3>
-                <p class="text-sm text-gray-500/90">Pengajuan "Investigasi Korupsi Daerah" Anda telah terkirim, mohon tunggu persetujuan dari Admin</p>
-                <span class="mt-1 flex gap-1 text-gray-500/90 text-xs">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                    8 hari yang lalu
-                </span>
-            </div>
-        </div>
-        <div class="flex gap-3.5">
-            <span class="py-1.5 px-1.5 h-fit bg-gray-200/60 text-indigo-600 rounded-lg">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                </svg>
-            </span>
-            <div class="flex flex-col gap-0.5">
-                <h3 class="text-base">Perbaikan pengajuan</h3>
-                <p class="text-sm text-gray-500/90">Pengajuan "Dewan Dorong Pemda dan APH Perkuat Pengawasan PETI" Anda butuh perbaikan. Lihat komentar Admin tentang perbaikan!</p>
-                <span class="mt-1 flex gap-1 text-gray-500/90 text-xs">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                    9 hari yang lalu
-                </span>
-            </div>
-        </div>
+    <div id="activitiesWrapper" class="activities mt-8 flex flex-col gap-4">
+        <?= view("components/activities", ["activities" => $activities]) ?>
     </div>
 </section>
 <!-- Akhir Section aktivitas terbaru -->
 <!-- Script Filter Box -->
 <script type="module" src="<?= base_url('/assets/js/filter-box.js') ?>"></script>
+<script src="<?= base_url('/assets/js/aktivitas.js') ?>"></script>
 <?= $this->endSection() ?>
