275bcf07a52bd8004ca981aaf4e78166d2fb2cda
fattahillah <fattahillahmuhammad48@gmail.com>
2026-04-12 20:27:43 +0700

    Mengubah Model UserMeta

--- app/Models/UserMeta.php
+++ app/Models/UserMeta.php
@@ -7,5 +7,21 @@ use CodeIgniter\Model;
 class UserMeta extends Model
 {
     protected $table = "user_meta";
-    protected $allowedFields = ["nama_lengkap", "user_id", "profil", "tanggal_lahir", "nomor_hp", "nama_media", "alamat_media", "last_login"];
+    protected $allowedFields = ["nama_lengkap", "profil_path_img", "kartu_tanda_anggota", "tanggal_lahir", "nomor_hp", "nama_media", "alamat_media", "last_login"];
+    public function getUserMeta(int $user_id, array|null $custom_fields = null)
+    {
+        $fields = [
+            "nama_lengkap",
+            "profil_path_img AS profil",
+            "kartu_tanda_anggota",
+            "tanggal_lahir",
+            "nomor_hp",
+            "nama_media",
+            "alamat_media",
+        ];
+        return $this
+            ->select($custom_fields ?? $fields)
+            ->where("user_id", $user_id)
+            ->first();
+    }
 }
